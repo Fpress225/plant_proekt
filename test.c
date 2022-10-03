fdbd9079e2ca39622c58d8f40137a5da7672ff52
Fpress225 <sidorovkirill320@gmail.com>
2022-10-03 13:08:01 +0300

    12

--- test.c
+++ test.c
@@ -3,19 +3,19 @@
 #include <stdlib.h>
 #include "Plant.h"
 
-plant *converting_a_string_to_a_plant_structure(char *string) {
+plant *parse_plant(char *string) {
     plant *pl;
     pl = malloc(sizeof(plant));
     char *sep = "^";
     pl->id = strtok(string, sep);
     pl->name = strtok(NULL, sep);
     pl->sort = strtok(NULL, sep);
-    printf("%d\n%s\n%s\n", &pl->id, &pl->name, &pl->sort);
+    printf("%d\n%s\n%s\n", pl->id, pl->name, pl->sort);
     return pl;
 }
 
 void main() {
     
 
-    converting_a_string_to_a_plant_structure("132^dggd^dgdgdd");
+    parse_plant("132^dggd^dgdgdd");
 }
